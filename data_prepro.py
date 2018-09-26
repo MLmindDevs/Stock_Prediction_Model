7abb7191d8887a40bbd27cf4ac747831dcc8442d
AlexPitteros <a.pitteros@gmail.com>
2018-09-26 03:56:55 +0300

    gfghh

--- data_prepro.py
+++ data_prepro.py
@@ -3,6 +3,7 @@ from collections import Counter
 import numpy as np
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+#alex is here biaaaaaatches
 
 def extract_data():
     data = pd.read_csv("data/RedditNews.csv") 
